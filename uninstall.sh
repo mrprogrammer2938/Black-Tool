96ffdd1ae5e0fe83b20fdfed16079e63618bcfa9
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-13 19:08:26 +0430

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,22 +4,19 @@ if [[ "$(id -u)" -ne 0 ]]; then
   echo "Please, Run This Programm as Root!"
   exit 1
 fi
-clear
-echo "Uninstalling..."
-sleep 2
-try_continue
 try_continue() {
+  clear
   echo "Do you want to Uninstalling Black-Tool? [y/n] "
   read try_to_uninstalling
   if [[ $try_to_uninstalling == 'y' ]]; then
     run
-  elsif [[ $try_to_uninstalling == 'n' ]]; then
+  elsif [[ $try_to_uninstalling == 'n' ]];
     clear
     echo "Thanks :)"
     echo ""
     exit 1
   else
-    try_continue()
+    try_continue
 }
 run() {
   cd .. && sudo rm -r Black-Tool
@@ -29,3 +26,7 @@ run() {
   echo ""
   exit 1
 }
+clear
+echo "Uninstalling..."
+sleep 2
+try_continue
