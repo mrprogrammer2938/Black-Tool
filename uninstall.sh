fea0a11c4fc4d77f4a1c63d894e5dd0fa352c630
Mr.nope <78996423+mrprogrammer2938@users.noreply.github.com>
2021-07-20 22:20:47 +0430

    Uninstalling
    uninstall

---
+++ uninstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# This code write by Mr.nope
+# Black-Tool 2.1.4
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo "Please, Run This Programm as Root!"
+  exit 1
+fi
+clear
+echo "Uninstalling..."
+sleep 2
+cd .. && rm -r Black-Tool && cd /usr/bin && rm -r hack
+echo ""
+echo "Finish...!"
+echo ""
+echo "Mr.nope: Good bye °^° "
+echo ""
+exit 1
