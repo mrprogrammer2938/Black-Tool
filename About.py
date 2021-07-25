f4f900560ada7573d5cc0ca604f660bee7b3dfb7
Mr.nope <78996423+mrprogrammer2938@users.noreply.github.com>
2021-07-25 22:29:52 +0430

    Create About.py

---
+++ About.py
@@ -0,0 +1,11 @@
+###############################################################################################
+# Developer: Sina Meysami                                                                     #
+# Code Name: Black-Tool                                                                       #
+# Version: 2.1.5                                                                              #
+# Github: https://github.com/mrprogrammer2938                                                 #
+# Instagram: https://instagram.com/mr.programmer2938                                          #
+# Pinterest: https://www.pinterest.com/mrprogrammer2938                                       #
+# Linkin: https://www.linkedin.com/in/programmer-programmer-95223a214/                        #
+# Twitter: Mrprogr61940982                                                                    #
+# Telegram: t.me/blacktm                                                                      #
+###############################################################################################
