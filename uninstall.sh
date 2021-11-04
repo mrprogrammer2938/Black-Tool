f279fa914b17ccef69efd9c77b27391fd6c37cb7
Mr.programmer2938 <78996423+mrprogrammer2938@users.noreply.github.com>
2021-11-04 19:24:06 +0330

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # This code write by Mr.Programmer2938
-# Black-Tool 4.0.0
+# Black-Tool v5.0.0
 # Uninstalling File
+#
+# Website: https://blacksoftware.ir
+# Github: https://github.com/black-software-com
+# Instagram: https://instagram.com/black_software_company
+#
 if [[ "$(id -u)" -ne 0 ]]; then
   echo "Please, Run This Programm as Root!"
   exit 1
